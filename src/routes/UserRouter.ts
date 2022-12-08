7841ebdbf07fb9440c4ef956fa4d88bbe1833503
ksiyeon27 <siyeonk27@gmail.com>
2022-12-09 03:43:15 +0900

    mypage 연결

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -4,14 +4,10 @@ import { UserController } from "../controllers";
 import auth from "../middleware/auth";
 const router: Router = Router();
 
-router.get("/signup", function (req, res) {
-  res.render("signup");
-});
 router.post("/signup", [], UserController.createUser);
-router.get("/signin", function (req, res) {
-  res.render("login");
-});
 router.post("/signin", UserController.signinUser);
 router.get("/", auth, UserController.getUser);
-
+router.get("/mypage", function (req, res) {
+  res.render("mypage");
+});
 export default router;
